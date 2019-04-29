56a0a188937c1581d711e4000b1a2bdf4bd792b3
Diego Martínez <dmartinezbaselga98@gmail.com>
2019-04-30 00:30:54 +0200

    leer fichero

--- practica1/p1.cpp
+++ practica1/p1.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <cstdio>
 
 using namespace std;
 
-int main()
+long tamanno_fichero(FILE *f)
 {
-    cout << "Hola" << endl;
+    fseek(f, 0, SEEK_END);
+    return ftell(f);
+}
+
+/*
+ * leer fichero, frecuencias[256], heap de arboles hoja != 0
+ *
+ */
+int main(int argc, char *argv[])
+{
+    FILE *f = fopen(argv[1], "rb");
+    long tam = tammano_fichero(f);
+    char *contenido = (char *)malloc(tamanno_fichero(f) + 1);
+    fread(contenido, tam, 1, f);
 }
