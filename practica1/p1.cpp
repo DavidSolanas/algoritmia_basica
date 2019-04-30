14b69d0fbfc20e873938fdaa40bbaea9ff3bb879
David <davidsolanas086@gmail.com>
2019-04-30 13:49:16 +0200

    Leer fichero arreglado y frec falta car especiales

--- practica1/p1.cpp
+++ practica1/p1.cpp
@@ -3,20 +3,47 @@
 
 using namespace std;
 
+/**
+ * Devuelve el tamaño del fichero f en Bytes.
+ */
 long tamanno_fichero(FILE *f)
 {
     fseek(f, 0, SEEK_END);
-    return ftell(f);
+    long tam = ftell(f);
+    fseek(f, 0, SEEK_SET);
+    return tam;
 }
 
-/*
- * leer fichero, frecuencias[256], heap de arboles hoja != 0
- *
+/**
+ * Lee el contenido del vector de caracteres y almacena
+ * en el vector frecuencias el número de apariciones de 
+ * cada carácter en dicho vector. La posición de un carácter
+ * en el vector frecuencias vendrá determinado por su código ASCII
  */
+void calcular_frecuencias(char *datos, int *frecuencias)
+{
+    int indice = 0;
+    while (datos[indice] != '\0')
+    {
+        cout << indice << ": " << (int)datos[indice] << endl;
+        frecuencias[datos[indice]]++;
+        indice++;
+    }
+}
+
 int main(int argc, char *argv[])
 {
     FILE *f = fopen(argv[1], "rb");
-    long tam = tammano_fichero(f);
-    char *contenido = (char *)malloc(tamanno_fichero(f) + 1);
+    long tam = tamanno_fichero(f);
+    char *contenido = (char *)malloc(tam + 1);
     fread(contenido, tam, 1, f);
+
+    // Creación del array e inicialización a 0
+    int frecuencias[256] = {0};
+    calcular_frecuencias(contenido, frecuencias);
+    for (int i = 0; i < 256; i++)
+    {
+        cout << i << ": " << frecuencias[i] << endl;
+    }
+    cout << endl;
 }
